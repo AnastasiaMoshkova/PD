4079444d8204d8ff92f92040418a9070fb9c4e21
polgusha <147660955+polgusha@users.noreply.github.com>
2023-10-11 23:16:56 +0300

    Add files via upload

---
+++ send_video_to_FeatureExtraction.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+os.chdir('C:\\Users\\Asus\\Desktop\\OpenFace_2.2.0_win_x64')
+
+def video_to_openface(path):
+    listLM=[]
+    listAU=[]
+    for file in os.listdir(path):
+        if 'mp4' in file:
+            if (file.split('.')[1]=='mp4'):
+                if (file.split('_')[0] in ['p2','p3','p5','p11']):
+                    listAU.append(file)
+
+
+    for i in range(len(listAU)):
+       subprocess.run('FeatureExtraction.exe -f ' + path + listAU[i] + ' -out_dir ' + path  + ' -aus')
+
+
+for i in ['44']:
+    path='C:\\Users\\Asus\\Desktop\\Patient\\patient'+i+'\\'+ 'face'+'\\'
+    video_to_openface(path)
+
